caa3c59539c394a6f6475def258819ac230e990b
Nathan Dunn <ndunn@me.com>
2019-09-13 13:18:27 -0700

    adding import

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+// import Rendering from '@gmod/jbrowse-plugin-svg/src/SvgFeatureRenderer/components/SvgFeatureRendering'
+import FeatureGlyph from '@gmod/jbrowse-plugin-svg/src/SvgFeatureRenderer/components/FeatureGlyph';
+import Rendering  from '@gmod/jbrowse-plugin-svg/src/SvgFeatureRenderer/components/SvgFeatureRendering';
+// import {FeatureGlyph,SvgFeatureRendering}  from '@gmod/jbrowse-plugin-svg/src/SvgFeatureRenderer/components/SvgFeatureRendering';
+// import {FeatureGlyph,SvgFeatureRendering}  from '@gmod/jbrowse-plugin-svg';
+// import {FeatureGlyph,SvgFeatureRendering}  from '@gmod/jbrowse-plugin-svg';
+// import {SvgFeatureRendering}  from '../../jbrowse-components/packages/svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        JBrowse Widget Example 1
-        <p>
-          Based on https://github.com/GMOD/jbrowse-components/tree/master/packages/protein-widget
-        </p>
-      </header>
+      Test
+      {/*<header className="App-header">*/}
+      {/*  JBrowse Widget Example 1*/}
+      {/*  <p>*/}
+      {/*    Based on https://github.com/GMOD/jbrowse-components/tree/master/packages/protein-widget*/}
+      {/*  </p>*/}
+      {/*</header>*/}
     </div>
   );
 }
