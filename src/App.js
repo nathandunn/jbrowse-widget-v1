480d3f3b543c00ea8458cb69b89f4ba6a04be1a4
Nathan Dunn <ndunn@me.com>
2019-09-13 13:19:21 -0700

    made it working again

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import Rendering from '@gmod/jbrowse-plugin-svg/src/SvgFeatureRenderer/components/SvgFeatureRendering'
-import FeatureGlyph from '@gmod/jbrowse-plugin-svg/src/SvgFeatureRenderer/components/FeatureGlyph';
-import Rendering  from '@gmod/jbrowse-plugin-svg/src/SvgFeatureRenderer/components/SvgFeatureRendering';
+// import FeatureGlyph from '@gmod/jbrowse-plugin-svg/src/SvgFeatureRenderer/components/FeatureGlyph';
+// import Rendering  from '@gmod/jbrowse-plugin-svg/src/SvgFeatureRenderer/components/SvgFeatureRendering';
 // import {FeatureGlyph,SvgFeatureRendering}  from '@gmod/jbrowse-plugin-svg/src/SvgFeatureRenderer/components/SvgFeatureRendering';
 // import {FeatureGlyph,SvgFeatureRendering}  from '@gmod/jbrowse-plugin-svg';
 // import {FeatureGlyph,SvgFeatureRendering}  from '@gmod/jbrowse-plugin-svg';
@@ -13,12 +13,12 @@ function App() {
   return (
     <div className="App">
       Test
-      {/*<header className="App-header">*/}
-      {/*  JBrowse Widget Example 1*/}
-      {/*  <p>*/}
-      {/*    Based on https://github.com/GMOD/jbrowse-components/tree/master/packages/protein-widget*/}
-      {/*  </p>*/}
-      {/*</header>*/}
+      <header className="App-header">
+        JBrowse Widget Example 1
+        <p>
+          Based on https://github.com/GMOD/jbrowse-components/tree/master/packages/protein-widget
+        </p>
+      </header>
     </div>
   );
 }
