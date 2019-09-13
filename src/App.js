940341c95b51809b51c03bf7ed736a1ce9d52a3a
Nathan Dunn <ndunn@me.com>
2019-09-13 14:47:48 -0700

    react lib import

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
 import React from 'react';
-// import Rendering from '@gmod/jbrowse-plugin-svg/src/SvgFeatureRenderer/components/SvgFeatureRendering'
-// import FeatureGlyph from '@gmod/jbrowse-plugin-svg/src/SvgFeatureRenderer/components/FeatureGlyph';
-// import Rendering  from '@gmod/jbrowse-plugin-svg/src/SvgFeatureRenderer/components/SvgFeatureRendering';
-// import {FeatureGlyph,SvgFeatureRendering}  from '@gmod/jbrowse-plugin-svg/src/SvgFeatureRenderer/components/SvgFeatureRendering';
-// import {SvgFeatureRendering}  from '@gmod/jbrowse-plugin-svg/src/SvgFeatureRenderer/components/SvgFeatureRendering';
 // import {FeatureGlyph,SvgFeatureRendering}  from '@gmod/jbrowse-plugin-svg';
 // import {FeatureGlyph,SvgFeatureRendering}  from '@gmod/jbrowse-plugin-svg';
 // import {SvgFeatureRendering}  from '../../jbrowse-components/packages/svg';
 
 import {Emoji} from 'mr-emoji'
-
-// import logo from './logo.svg';
 import './App.css';
+// import Rendering from '@gmod/jbrowse-plugin-svg/src/SvgFeatureRenderer/components/SvgFeatureRendering'
+// import FeatureGlyph from '@gmod/jbrowse-plugin-svg/src/SvgFeatureRenderer/components/FeatureGlyph';
+// import Rendering  from '@gmod/jbrowse-plugin-svg/src/SvgFeatureRenderer/components/SvgFeatureRendering';
+// import {FeatureGlyph,SvgFeatureRendering}  from '@gmod/jbrowse-plugin-svg/src/SvgFeatureRenderer/components/SvgFeatureRendering';
+// import {NCListAdapter}  from '@gmod/jbrowse-plugin-jbrowse1/src/index';
+// import NCListAdapter  from '@gmod/jbrowse-core';
+// import {NCListAdapter}  from '@gmod/jbrowse-core/mst-types';
+// import {MstTypes}  from '@gmod/jbrowse-core/mst-types';
+// import {NCListAdapter}  from '@gmod/jbrowse-plugin-jbrowse1';
+// import {NCListAdapter}  from '@gmod/jbrowse-plugin-jbrowse1/src/NCListAdapter';
+// const SvgFeatureRendering = require('@gmod/jbrowse-plugin-svg/src/SvgFeatureRenderer/components/SvgFeatureRendering');
+// const {FeatureGlyp,SvgFeatureRendering} = require('@gmod/jbrowse-plugin-svg');
+import ExampleComponent from 'reacttestlib';
 
 function App() {
   return (
     <div className="App">
       Test
-      <header className="App-header">
+      {/*<header className="App-header">*/}
         JBrowse Widget Example 1
         <Emoji emoji=':santa::skin-tone-3:' size={16} />
         <p>
           Based on https://github.com/GMOD/jbrowse-components/tree/master/packages/protein-widget
         </p>
-      </header>
+      {/*</header>*/}
+      <ExampleComponent text='Modern React component module' />
     </div>
   );
 }
